20566986e8a8dbf4b3c5e750ecab4317001ce73b
Arya Salian <arya.s.salian@gmail.com>
2024-08-09 00:57:14 +0530

    100% space station done & regi links, responsive needed

--- static/js/dolly-zoom.js
+++ static/js/dolly-zoom.js
@@ -9,13 +9,16 @@ document.addEventListener('scroll', function() {
     const screen = document.querySelector('#screen');
     const coffee = document.querySelector('#coffee');
     const image = document.querySelector('.starry-sky');
+    const ramplanet = document.querySelector('#ram-planet');
+    const hacknctext = document.querySelector('#HackNC-text');
+    const bannerinfo = document.querySelector('.banner-info')
     const scrollPosition = window.scrollY;
 
     const initialRamSize = 50;
     const initialRamY = -90
 
-    const initCtrl = 40; // 40vw height
-    const initCtrlX = 0;
+    const initCtrl = 30; // 40vw height
+    const initCtrlX = -10;
     
     const initPipe = 40; // 40vw height
     const initPipeX = 10;
@@ -31,6 +34,12 @@ document.addEventListener('scroll', function() {
     const initCoffee = 10; // 10vw height
     const initCoffeeX = -170;
     const initCoffeeY  = -150;
+
+    const initPlanet = 40; // 40vw height
+    const initPlanetX = 0;
+
+    const initText = 40; // 40vw height
+    const initTextX = 0;
 
     const initialFrameSize = 110;
     const initialImageSize = 150;
@@ -55,13 +64,19 @@ document.addEventListener('scroll', function() {
     const newIDX = initIDX + (scrollPosition/ (maxStickyScroll/(-350-initIDX)))
     const newIDY = initIDY + (scrollPosition/ (maxStickyScroll/(300-initIDY)))
 
-    const newScreen = initScreen + (scrollPosition / (maxStickyScroll/(90-initScreen)))
-    const newScreenX = initScreenX + (scrollPosition/ (maxStickyScroll/(200-initScreenX)))
-    const newScreenY = initScreenY + (scrollPosition/ (maxStickyScroll/(90-initScreenY)))
+    const newScreen = initScreen + (scrollPosition / (maxStickyScroll/(80-initScreen)))
+    const newScreenX = initScreenX + (scrollPosition/ (maxStickyScroll/(240-initScreenX)))
+    const newScreenY = initScreenY + (scrollPosition/ (maxStickyScroll/(35-initScreenY)))
 
-    const newCoffee = initCoffee + (scrollPosition / (maxStickyScroll/(70-initCoffee)))
-    const newCoffeeX = initCoffeeX + (scrollPosition/ (maxStickyScroll/(650-initCoffeeX)))
-    const newCoffeeY = initCoffeeY + (scrollPosition/ (maxStickyScroll/(600-initCoffeeY)))
+    const newCoffee = initCoffee + (scrollPosition / (maxStickyScroll/(60-initCoffee)))
+    const newCoffeeX = initCoffeeX + (scrollPosition/ (maxStickyScroll/(1000-initCoffeeX)))
+    const newCoffeeY = initCoffeeY + (scrollPosition/ (maxStickyScroll/(700-initCoffeeY)))
+    
+    const newPlanet = initPlanet + (scrollPosition / (maxStickyScroll/(50-initPlanet)))
+    const newPlanetX = initPlanetX + (scrollPosition/ (maxStickyScroll/(-15-initPlanetX)))
+
+    const newText = initText + (scrollPosition / (maxStickyScroll/(45-initText)))
+    const newTextX = initTextX + (scrollPosition/ (maxStickyScroll/(15-initTextX)))
 
     // Apply the new sizes
     if (scrollPosition <= maxStickyScroll) {
@@ -79,21 +94,34 @@ document.addEventListener('scroll', function() {
         screen.style.transform = `translate(${newScreenX}%, ${newScreenY}%)`;
         coffee.style.height = `${newCoffee}vw`; // keep as vw
         coffee.style.transform = `translate(${newCoffeeX}%, ${newCoffeeY}%)`;
+        ramplanet.style.height = `${newPlanet}vw`; // keep as vw
+        ramplanet.style.transform = `translate(${newPlanetX}%, -50%)`;
+        hacknctext.style.height = `${newText}vw`; // keep as vw
+        hacknctext.style.transform = `translate(${newTextX}%, -70%)`;
+        bannerinfo.style.opacity = `0`;
+        bannerinfo.style.visibility = `hidden`;
     }
     // To ensure render issues don't happen due to scrolling too fast:
     else {
         frame.style.backgroundSize = `${maxFrameBGSize}vw ${maxFrameBGSize}vh`;
         image.style.backgroundSize = `${minImageBGSize}vw ${minImageBGSize}vh`;
         spaceram.style.width = `300vw`;
+        spaceram.style.transform = `translate(-50%, 500%)`;
         ctrl.style.height = `100vw`; // keep as vw
         ctrl.style.transform = `translate(-300%, -50%)`;
         pipes.style.height = `100vw`; // keep as vw
         pipes.style.transform = `translate(400%, -50%)`;
         ID.style.height = `80vw`; // keep as vw
         ID.style.transform = `translate(-350%, 300%)`;
-        screen.style.height = `90vw`; // keep as vw
-        screen.style.transform = `translate(200%, 90%)`;
-        coffee.style.height = `70vw`; // keep as vw
-        coffee.style.transform = `translate(650%, 600%)`;
+        screen.style.height = `80vw`; // keep as vw
+        screen.style.transform = `translate(240%, 35%)`;
+        coffee.style.height = `60vw`; // keep as vw
+        coffee.style.transform = `translate(1000%, 700%)`;
+        ramplanet.style.height = `50vw`; // keep as vw
+        ramplanet.style.transform = `translate(-15%, -50%)`;
+        hacknctext.style.height = `45vw`; // keep as vw
+        hacknctext.style.transform = `translate(15%, -70%)`;
+        bannerinfo.style.opacity = 1;
+        bannerinfo.style.visibility = 'visible';
     }
 });
